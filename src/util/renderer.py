fc77be38b737f0c1ad631b7c5dfbd0fbe64fd0ae
James Cotton <peabody124@gmail.com>
2019-09-29 11:17:54 -0500

    Recent CV expects colors as list

--- src/util/renderer.py
+++ src/util/renderer.py
@@ -219,8 +219,7 @@ def render_model(verts,
     if color_id is None:
         color = colors['light_blue']
     else:
-        color_list = colors.values()
-        color = color_list[color_id % len(color_list)]
+        color = colors[list(colors)[color_id]]
 
     imtmp = simple_renderer(rn, verts, faces, color=color)
 
@@ -292,19 +291,19 @@ def draw_skeleton(input_image, joints, draw_edges=True, vis=None, radius=None):
         radius = max(4, (np.mean(input_image.shape[:2]) * 0.01).astype(int))
 
     colors = {
-        'pink': np.array([197, 27, 125]),  # L lower leg
-        'light_pink': np.array([233, 163, 201]),  # L upper leg
-        'light_green': np.array([161, 215, 106]),  # L lower arm
-        'green': np.array([77, 146, 33]),  # L upper arm
-        'red': np.array([215, 48, 39]),  # head
-        'light_red': np.array([252, 146, 114]),  # head
-        'light_orange': np.array([252, 141, 89]),  # chest
-        'purple': np.array([118, 42, 131]),  # R lower leg
-        'light_purple': np.array([175, 141, 195]),  # R upper
-        'light_blue': np.array([145, 191, 219]),  # R lower arm
-        'blue': np.array([69, 117, 180]),  # R upper arm
-        'gray': np.array([130, 130, 130]),  #
-        'white': np.array([255, 255, 255]),  #
+        'pink': [197, 27, 125],  # L lower leg
+        'light_pink': [233, 163, 201],  # L upper leg
+        'light_green': [161, 215, 106],  # L lower arm
+        'green': [77, 146, 33],  # L upper arm
+        'red': [215, 48, 39],  # head
+        'light_red': [252, 146, 114],  # head
+        'light_orange': [252, 141, 89],  # chest
+        'purple': [118, 42, 131],  # R lower leg
+        'light_purple': [175, 141, 195],  # R upper
+        'light_blue': [145, 191, 219],  # R lower arm
+        'blue': [69, 117, 180],  # R upper arm
+        'gray': [130, 130, 130],  #
+        'white': [255, 255, 255],  #
     }
 
     image = input_image.copy()
